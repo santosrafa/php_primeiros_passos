cc5f9fd405d36eac6df043c954b13aa94a52599e
Your Name <ti.rafaelteles@gmail.com>
2022-06-28 19:53:33 -0300

    operacoes_matematica

---
+++ operacoes_matematica.php
@@ -0,0 +1,15 @@
+<?php
+
+/* EM OPERACOES MATEMATICAS O PHP TRABALHA VÁRIAS, MAS ABAIXO ESTARAO AS PRINCIPAIS
+SOMA, SUBTRACAO, DIVISÃO, MULTIPLICACAO, CUBO E RESTO DA DIVISAO */
+
+$soma = 2+2;
+$subtracao = 2-2;
+$multiplicacao = 2*2;
+$divisao = 10/2;
+
+$dois_ao_cubo = 2**3;
+
+$resto_da_divisao = 10 % 3;
+
+echo $divisao;
