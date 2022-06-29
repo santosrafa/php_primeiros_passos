88f785cafcacea3f96e67efb4b9129dd559d503c
Your Name <ti.rafaelteles@gmail.com>
2022-06-29 20:03:10 -0300

    alteracoes

---
+++ tipos_de_dados.php
@@ -0,0 +1,12 @@
+<?php
+
+$idade = 31;                    /* VALOR INTEIRO */
+
+$salario = 31.10;               /* VALOR DECIMA */
+
+$texto = "Ola Mundo";           /* VALOR DO TIPO STRING */
+
+$verdadeiro = true;             /* VALOR DO TIPO BOLEANO */
+
+echo gettype($verdadeiro);      /* GETTYPE IDENTIFICA O TIPO DA VARIAVEL */
+
