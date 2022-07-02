4b259f85fe68f902932a18eb14185d980d456ac0
Your Name <ti.rafaelteles@gmail.com>
2022-07-02 14:09:12 -0300

    tipos_strings

---
+++ tipos_string.php
@@ -0,0 +1,8 @@
+<?php
+
+$idade = 21;
+
+echo "Ola Mundo! Minha idade é $idade anos";
+
+/* O PONTO NO PHP CONCATENA STRING  */
+/* UTILIZANDO ASPAS DUPLAS O PHP ENTENDE QUE $IDADE É UMA VARIÁVEL. */
