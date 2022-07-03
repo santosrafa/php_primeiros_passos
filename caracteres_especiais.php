3fa47ac4925bb31c67d5e11e33d25307b42b2996
Your Name <ti.rafaelteles@gmail.com>
2022-07-03 10:38:17 -0300

    caracteres_especiais

---
+++ caracteres_especiais.php
@@ -0,0 +1,7 @@
+<?php
+
+$idade = 21;
+
+echo "Olá Mundo!!\n";                               /* O \n REALIZA A QUEBRA DE LINHA */
+echo "Eu tenho $idade anos";                        /* O \t EXECUTA UM TAB NA PALAVRA */
+echo "Vamos realizar mais testes" . PHP_EOL;        /* PHP_EOL EXERCE A MESMA FUNCAO DO \N PORÉM É MAIS SIMPLES DE ENTENDER */
