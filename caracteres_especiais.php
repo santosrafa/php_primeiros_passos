166b1821dfdd78c3eef38bbc3f3cf8f24598c8c7
Your Name <ti.rafaelteles@gmail.com>
2022-07-03 10:42:37 -0300

    caracteres_especiais

--- caracteres_especiais.php
+++ caracteres_especiais.php
@@ -4,4 +4,4 @@ $idade = 21;
 
 echo "Olá Mundo!!\n";                               /* O \n REALIZA A QUEBRA DE LINHA */
 echo "Eu tenho $idade anos";                        /* O \t EXECUTA UM TAB NA PALAVRA */
-echo "Vamos realizar mais testes" . PHP_EOL;        /* PHP_EOL EXERCE A MESMA FUNCAO DO \N PORÉM É MAIS SIMPLES DE ENTENDER */
+echo "Vamos realizar mais testes" . PHP_EOL;        /* PHP_EOL EXERCE A MESMA FUNCAO DO \N PORÉM A VANTAGEM É DEIXAR O CODIGO FUNCIONANDO EM VARIAS PLATAFORMAS */
