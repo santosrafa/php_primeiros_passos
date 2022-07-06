5161a4d66445c1b22a7ec224bb2025b90ed66830
Your Name <ti.rafaelteles@gmail.com>
2022-07-06 19:09:29 -0300

    decisoes

---
+++ decisoes.php
@@ -0,0 +1,15 @@
+<?php
+
+$idade = 21;
+
+echo "Você só pode entrar se tiver a partir de 18 anos." . PHP_EOL;
+
+if ($idade >= 18) {
+    echo "Você tem $idade anos" . PHP_EOL;
+    echo "Você pode entrar!";
+}
+
+/* IF É UMA FORMA DE DECISAO
+
+|| = AND
+&& = OR */
