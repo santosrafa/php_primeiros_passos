6ad137193c85ef5668478b4d7a5575bcdba5e964
Your Name <ti.rafaelteles@gmail.com>
2022-07-12 21:48:56 -0300

    repeticao2

---
+++ repeticao2.php
@@ -0,0 +1,12 @@
+<?php
+
+for ($contador = 1; $contador <= 15; $contador++){
+    echo "#$contador" . PHP_EOL;
+}
+
+/* ---UTILIZANDO FOR---
+
+DUAS FORMAS DE ESCREVER A MESMA COISA $contador ++
+$contador = $contador + 1;
+$contador += $contador;
+$contador ++ */
