a2d12755c2b486e5bc40d58d2a15b0400abd16cc
Your Name <ti.rafaelteles@gmail.com>
2022-07-13 23:03:19 -0300

    desafio_numeros_impares

---
+++ desafio_numeros_impares.php
@@ -0,0 +1,9 @@
+<?php
+
+for ($contador = 1; $contador < 100; $contador ++){             /* SE O CONTADOR FOR < 100 EXECUTE O LOOP */
+    if ($contador % 2 != 0){                                    /* SE O RESTO DA DIVISAO FOR IGUAL 0 */
+        echo $contador . PHP_EOL;
+    }
+}
+
+/* DESAFIO PARA APRESENTAR NUMEROS IMPARES */
