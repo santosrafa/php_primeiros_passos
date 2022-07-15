d2dac90f7d9b1b44e2098dc4569030e53106d592
Your Name <ti.rafaelteles@gmail.com>
2022-07-14 22:50:27 -0300

    desafio_tabuada

---
+++ desafio_tabuada.php
@@ -0,0 +1,10 @@
+<?php
+
+$multiplicador = 3;
+
+for ($i = 1; $i <= 10; $i++){
+    $resultado = $multiplicador * $i;
+    echo "$multiplicador x $i = $resultado" . PHP_EOL;
+}
+
+/* DESAFIO TABUADA */
